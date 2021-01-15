4f772c51af98dc1264188b4995ddffa9d89761e8
Amanda Nelson <amandaln4311@gmail.com>
2021-01-14 19:54:04 -0500

    questions, functions, if else completed

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application;
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+{
+    type: 'input',
+    name: 'github',
+    message: 'What is your Github username?',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+},
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project',
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'NONE'],
+},
+{
+    type: 'input',
+    name: 'installation', 
+    message: 'What command should be run to install dependencies?',
+},
+{
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?',
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?',
+}])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//  TODO: Create a function to write README file
+// getting input back
+.then((answers) => {
+    console.log(answers);
+    const userInput = generateMarkdown(answers);
+//    actually write the file
+    fs.writeFile("ReadMe.md", userInput, (err) =>
+    err ? console.log(err) : console.log('Success!')
+);
+})
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,84 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+const renderLicenseBadge = (license) => {
+  
+  if (license === 'APACHE 2.0' ) license = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
+  if (license === 'MIT' ) license ='[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+  if (license === 'GPL 3.0' ) license ='[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
+  if (license === 'BSD 3' ) license ='[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]';
+  if (license === 'NONE' ) license ='';
+
+
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+const renderLicenseLink = (license) => {
+  if (license === 'APACHE 2.0') license = '(https://opensource.org/licenses/Apache-2.0)';
+  if (license === 'MIT') license = '(https://opensource.org/licenses/MIT)';
+  if (license === 'GPL 3.0') license = '(https://www.gnu.org/licenses/gpl-3.0)';
+  if (license === 'BSD 3') license = '(https://opensource.org/licenses/BSD-3-Clause)';
+  if (license === 'NONE' ) license ='';
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+const renderLicenseSection = (license) => {
+  return `## License ${license}`;
+}
+
+// TODO: Create a function to generate markdown for README
+const generateMarkdown = (data) => {
+ const liceSection = renderLicenseSection(data.license);
+ const liceBadge = renderLicenseBadge(data.license);
+ const liceLink = renderLicenseLink(data.license);
+
+
+  
+  
+  return `# ${data.title}
+
+​   ## Description
+${data.description}
+​
+
+ ## Table of Contents 
+​
+* [Installation](#installation)
+​
+* [Usage](#usage)
+​
+* [License](#license)
+​
+* [Contributing](#contributing)
+​
+* [Tests](#tests)
+​
+* [Questions](#questions)
+​
+## Installation
+​
+${data.installation}
+
+​
+## Usage
+​${data.usage}
+
+​
+${liceSection}
+
+  
+ ## Contributing
+​ ${data.contributing}
+
+## Tests
+${data.test}
+
+## Questions
+​
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+  `;
+
+}
+
+module.exports = generateMarkdown;
